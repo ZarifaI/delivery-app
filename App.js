a7bd2fddf0057afeb5569fb7d7416437b398e4d7
Zarifa <121843967+ZarifaI@users.noreply.github.com>
2024-06-08 18:12:02 -0400

    Created restaurant screen which gives solid information about specific restaurant; its dishes, address, rating, alergy information.

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {TailwindProvider} from "tailwindcss-react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
+import RestaurantScreen from './screens/RestaurantScreen';
 
 const Stack = createNativeStackNavigator();
 
